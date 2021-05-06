9b0840cc491d5e9f9d04c9993057048b3c4acb21
Ruris <ruris@qq.com>
2021-05-06 16:21:48 +0800

    Update LLWebViewBridge.swift

--- LLWebViewController/Classes/LLWebViewBridge.swift
+++ LLWebViewController/Classes/LLWebViewBridge.swift
@@ -11,12 +11,16 @@ import WKWebViewJavascriptBridge
 
 open class LLWebViewBridge: NSObject, LLWebViewBridgable {
     
+    /// requestURL
     open var requestURL: URL {
         fatalError("LLWebViewBridge 的 requestURL 不能为 nil")
     }
     
+    /// WKWebViewJavascriptBridge
     public var bridge: WKWebViewJavascriptBridge
     
+    /// Init
+    /// - Parameter webView: WKWebView
     public required init(webView: WKWebView) {
         bridge = WKWebViewJavascriptBridge(webView: webView)
     }
